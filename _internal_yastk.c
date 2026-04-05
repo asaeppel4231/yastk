a3c98c4449706cdec4d442d994dcc5a856a0d86b
asaeppel4231 <asaeppel4231@gmx.de>
2026-04-05 15:49:18 +0200

    Implement many internal functions in a first version and add option flags

--- _internal_yastk.c
+++ _internal_yastk.c
@@ -13,11 +13,16 @@ struct yastk{
     size_t size;
 };
 
-
-// TODO: Unpause implementing
-
 size_t    _internal_get_yastk_entry_struct_size(){
     return sizeof(yastk_entry_t);
+}
+
+void       _internal_helper_call_free_func_yastk(free_func func, void* data){
+    if(func != NULL){
+        if(data != NULL){
+            func(data);
+        }
+    }
 }
 
 void      _internal_init_yastk_entry(yastk_entry_t* entry){
@@ -25,18 +30,19 @@ void      _internal_init_yastk_entry(yastk_entry_t* entry){
     _internal_set_next_yastk_entry(entry, NULL, NULL);
 }
 
-int       _internal_free_yastk_entry(yastk_entry_t* entry, free_func func){
-    if(func != NULL){
-        func(_internal_get_yastk_entry_data(entry));
-    }
+void     _internal_free_yastk_entry(yastk_entry_t* entry, free_func func){
+    _internal_helper_call_free_func_yastk(func, _internal_get_yastk_entry_data(entry));
+    free(entry);
 }
 
-int       _internal_free_yastk_entries(yastk_entry_t* entry, free_func func){
+void     _internal_free_yastk_entries(yastk_entry_t* entry, free_func func){
     yastk_entry_t* current;
     yastk_entry_t* next;
     current = entry;
-    while(current != NULL ){
-
+    while(current != NULL){
+        next = _internal_get_next_yastk_entry(current);
+        _internal_free_yastk_entry(current, func);
+        current = next;   
     }
 }
 
@@ -72,14 +78,43 @@ size_t    _internal_get_yastk_struct_size(){
     return sizeof(yastk_t);
 }
 
-int       _internal_init_yastk(yastk_t* stack){
-    //stack->head = malloc(_internal);
+void     _internal_init_yastk(yastk_t* stack, int opts){
+    stack->head = NULL;
+    stack->tail = NULL;
+    stack->size = 0;
 }
-void      _internal_free_yastk(yastk_t* stack);
 
-int       _internal_yastk_push(yastk_t* stack, void* data, int opts);
-void*     _internal_yastk_pop (yastk_t* stack, int opts);
+void     _internal_free_yastk(yastk_t* stack, int opts){
+    _internal_free_yastk_entries(stack->head, NULL); /* FIXME: Register a free function in the structure*/
+    free(stack);
+}
 
-size_t    _internal_get_yastk_size(yastk_t* stack){
+void      _internal_push_yastk(yastk_t* stack, void* data, int opts){
+    yastk_entry_t* temp = malloc(_internal_get_yastk_entry_struct_size());
+    if(temp == NULL){
+        return;
+    }
+
+    _internal_init_yastk_entry(temp);
+    _internal_set_yastk_entry_data(temp, data, NULL); /* FIXME: Save a free function in the structure and use that*/
+    _internal_set_next_yastk_entry(stack->tail, temp, NULL);
+
+}
+
+void     _internal_pop_yastk(yastk_t* stack, void** out_data, int opts){
+    void* result = _internal_get_yastk_entry_data(stack->tail);
+    yastk_entry_t* temp = stack->head;
+    while(temp != NULL && _internal_get_next_yastk_entry(temp) != stack->tail){ /* TODO: Use a helper pointer instead*/
+        temp = _internal_get_next_yastk_entry(temp);
+    }
+    _internal_free_yastk_entry(stack->tail, NULL); /* See Line 101 and 90 */
+    _internal_set_next_yastk_entry(temp, NULL, NULL);
+    
+    stack->tail = temp;
+    
+    *out_data = result;
+}
+
+size_t    _internal_get_size_yastk(yastk_t* stack, int opts){
     return stack->size;
 }
--- _internal_yastk.h
+++ _internal_yastk.h
@@ -3,16 +3,24 @@
 
 #include <stddef.h>
 
+#define YASTK_NO_OPTS 0
+#define YASTK_STUB 1
+#define YASTK_PROVOCATE_ERROR 2
+
+#define YASTK_STUB_RETVAL 0
+
 typedef struct yastk_entry yastk_entry_t;
 
 size_t    _internal_get_yastk_entry_struct_size();
 
 typedef void(*free_func)(void*);
 
-void       _internal_init_yastk_entry(yastk_entry_t* entry);
-int       _internal_free_yastk_entry(yastk_entry_t* entry, free_func func);
+void       _internal_helper_call_free_func_yastk(free_func func, void* data);
 
-int       _internal_free_yastk_entries(yastk_entry_t* entry, free_func func);
+void      _internal_init_yastk_entry(yastk_entry_t* entry);
+
+void     _internal_free_yastk_entry(yastk_entry_t* entry, free_func func);
+void     _internal_free_yastk_entries(yastk_entry_t* entry, free_func func);
 
 void            _internal_set_next_yastk_entry(yastk_entry_t* entry, yastk_entry_t* next, free_func next_free_func);
 yastk_entry_t* _internal_get_next_yastk_entry(yastk_entry_t* entry);
@@ -24,12 +32,14 @@ typedef struct yastk yastk_t;
 
 size_t    _internal_get_yastk_struct_size();
 
-int       _internal_init_yastk(yastk_t* stack);
-void      _internal_free_yastk(yastk_t* stack);
+void     _internal_init_yastk(yastk_t* stack, int opts);
+void     _internal_free_yastk(yastk_t* stack, int opts);
 
-int       _internal_yastk_push(yastk_t* stack, void* data, int opts);
-void*     _internal_yastk_pop (yastk_t* stack, int opts);
+void      _internal_yastk_push(yastk_t* stack, void* data, int opts);
+void     _internal_yastk_pop (yastk_t* stack, void** out_data, int opts);
 
-size_t    _internal_get_yastk_size(yastk_t* stack);
+size_t    _internal_get_size_yastk(yastk_t* stack, int opts);
+yastk_entry_t* _internal_get_head_yastk(yastk_t* stack, int opts);
+yastk_entry_t* _internal_get_tail_yastk(yastk_t* stack, int opts);
 
 #endif
